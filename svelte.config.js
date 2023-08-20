e2a00035fa7468fbc458ea8ce71f229604a47637
Masaya Shida <masayashida@gmail.com>
2023-08-21 01:47:14 +0700

    update adapter static config

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
