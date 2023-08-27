5cdcd164776847849077c0f844a8512e38340f9b
Masaya Shida <masayashida@gmail.com>
2023-08-27 16:04:40 +0700

    remove supabase client

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
-import type { Database, Json } from '../supabase';
-
-export type { Database, Json };
-
 export type Section = {
 	created_at: string;
 	id: number;
