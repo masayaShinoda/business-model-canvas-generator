4eac00dc354a87b2e45e457b0c47e58e54f6e68c
Masaya Shida <masayashida@gmail.com>
2023-09-04 02:00:10 +0700

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,30 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+	let language: 'kh' | 'en'
+	$: language = $page.url.pathname.startsWith('/kh') ? 'kh' : 'en';
+</script>
 <footer>
 	<p class="version">Version: beta 1.2</p>
+	<a href="https://www.buymeacoffee.com/masayashida">Buy me a coffee ☕</a>
+	<br>
 	<a href="https://masayashida.com">masayashida.com</a>
 </footer>
 
 <style>
 	footer {
-		color: var(--clr_grey_shade_b);
 		max-width: var(--content_max_width);
 		padding: var(--content_padding_horizontal);
-		margin: 2.5rem auto 1.5rem auto;
+		margin: 2.5rem auto 5rem auto;
 		flex: 1;
 	}
-	footer > p {
+	footer .version {
 		margin: 0;
+		color: var(--clr_grey_shade_b);
+	}
+	[data-theme="dark"] footer .version {
+		color: var(--clr_grey_shade_d);
+	}
+	footer a {
+		text-decoration: none;
 	}
 </style>
